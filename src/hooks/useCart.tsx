00647f9f3286df723aa4162b114fcf80db3b02e9
MatiasPedro25 <unbmatiaspedro@gmail.com>
2021-03-22 11:37:08 -0300

    Challenge Completed!

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -33,53 +33,53 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   });
 
   const addProduct = async (productId: number) => {
-    let cartTemp = [...cart] //easy to handle temporary array
-    const productIndex = cartTemp.map(prod=>prod.id).indexOf(productId) 
+    let cartTemp = cart //easy to handle temporary array
+    let desiredAmount = null; // amount to be added
 
     try {
-      const query = await api.get('/stock')
-      const stock:Stock[] = query.data //fetching stock data
-      const response = await api.get(`/products/${productId}`) //search for prod to be added
-      const desiredProduct = response.data
-
-      const productInStock = stock.find(prod=>prod.id===productId) //Find specific product in stock
-      const productPrevAmount = productIndex === -1? 0 : cartTemp[productIndex].amount
-
-      if(productInStock && productInStock.amount > productPrevAmount){
+      //Fetching product specs in stock
+      const productInStock:Stock = (await api.get(`/stock/${productId}`)).data
+      //Trying to find this product in the cart 
+      const productIndex = cartTemp.map(prod=>prod.id).indexOf(productId)
+      //Stablishing amount to be added based on product's first appearence
+      desiredAmount = productIndex === -1 ? 1: cartTemp[productIndex].amount + 1
+      //if there is enough in stock
+      if(productInStock.amount >= desiredAmount){
         if(productIndex===-1){ //First time adding this product
-          cartTemp.push({...desiredProduct,amount:1})
-        }else{//Product's been chosen already
-          cartTemp[productIndex].amount += 1
+          const desiredProduct = (await api.get(`/products/${productId}`)).data
+          cartTemp.push({...desiredProduct,amount:desiredAmount})
+        }else{ //Product already been chosen
+          cartTemp[productIndex].amount = desiredAmount
         }
-        setCart(cartTemp)
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(cartTemp)) 
+        setCart([...cartTemp])
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart)) 
       }
+      //Not enough in stock
       else{
-        toast.error('Quantidade solicitada fora de estoque')
+        throw new Error('Quantidade solicitada fora de estoque')
       }
-    } catch(error) {
-      if(error.message)
-        toast.error('Erro na adição do produto')
+    } 
+    catch(error) {
       toast.error(error.message)
+      toast.error('Erro na adição do produto')
     }
   };
 
   const removeProduct = (productId: number) => {
-    let cartTemp = [...cart]// easy to handle temporary array
+    let cartTemp = cart// easy to handle temporary array
     try {
-      if(!cartTemp.find(prod=>prod.id!==productId))
-        throw new Error("Erro na remoção do produto")
-      cartTemp = cartTemp.filter(prod=>prod.id!==productId)
-      setCart([...cartTemp])
-      if(cartTemp.length>0)
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(cartTemp))
-      else
-        localStorage.removeItem('@RocketShoes:cart')
-    } catch(error) {
-      if(error.response) {
-        toast.error("Erro na remoção do produto");
+      if((cartTemp.filter(prod=>prod.id===productId)).length > 0){
+        cartTemp = cartTemp.filter(prod=>prod.id!==productId)
+        setCart([...cartTemp])
+        if(cartTemp.length>0)
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(cartTemp))
+        else
+          localStorage.removeItem('@RocketShoes:cart')
+      }else{ //This product does not exist in our DB
+        throw new Error('Erro na remoção do produto')
       }
-    
+    }
+    catch(error) {
       toast.error(error.message)
     }
   };
@@ -88,21 +88,31 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     productId,
     amount,
   }: UpdateProductAmount) => {
+    let cartTemp = cart //easier-to-handle cart copy
     try {
-      const query = await api.get('/stock')
-      const stock:Stock[] = query.data //fetching stock data
-      const productInStock = stock.find(prod=>prod.id===productId)
-      if(productInStock && (productInStock.amount<amount)){// Not enough in stock
-        throw new Error()
-      }
-      let cartTemp = cart //easy to handle temporary array
+      if(amount<=0)
+        throw new Error('Erro na alteração da quantidade do produto')
+      //If this product is actually in the cart
+      if((cartTemp.filter(prod=>prod.id===productId)).length > 0){
+      //Fetching product specs in stock
+      const productInStock:Stock = (await api.get(`/stock/${productId}`)).data
+      // Not enough in stock
+      if(productInStock && (productInStock.amount<amount))
+        throw new Error('Quantidade solicitada fora de estoque')
+      //Finding product's position in array
       const productIndex = cartTemp.map(prod=>prod.id).indexOf(productId)
+      //Updating its amount
       cartTemp[productIndex].amount = amount
+      //Updating cart state
       setCart([...cartTemp])
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cartTemp))
-
+      //Storing cart in localStorage
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+      }
+      else{ //This product does not exist in our DB
+        throw new Error('Erro na alteração de quantidade do produto')
+      }   
     } catch(error) {
-      toast.error('Erro na alteração da quantidade do produto')
+      toast.error(error.message)
     }
   };
 
